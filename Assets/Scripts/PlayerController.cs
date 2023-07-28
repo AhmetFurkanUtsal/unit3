de84401edbd87d13d51070165f006ecf17b21596
AhmetFurkanUtsal <furkan.utsal@hacettepe.edu.tr>
2023-07-28 16:12:03 +0300

    v1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier; // Physics.gravity = Physics.gravity * gravitYModifier
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce,ForceMode.Impulse);
+            // Addforce gerçek hayatta cismin kütlesini aldı ve yukarı yöde hareket ettirdi.
+            // ForceMode.Impulse anında uygular
+            isOnGround = false;
+            // buraya kadar yaptığımızda oyuncu bir kez zıplıyor. 
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+       isOnGround=true;
+        //oyuncunun havada veya yerde olduğunu bilmiyoruz
+        // yere çarptığı anda tekrar true yaptık
+    }
+}
